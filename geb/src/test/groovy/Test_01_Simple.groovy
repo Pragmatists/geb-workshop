f24fcd24a99784b923d8ee2e88afca502b0f6f1a
Adam Ruka <adam.ruka@pragmatists.pl>
2014-09-24 16:40:51 +0200

    Show reporting in Geb.

--- geb/src/test/groovy/Test_01_Simple.groovy
+++ geb/src/test/groovy/Test_01_Simple.groovy
@@ -1,13 +1,15 @@
-import geb.junit4.GebTest
+import geb.junit4.GebReportingTest
 import org.junit.Test
 
-class Test_01_Simple extends GebTest {
+class Test_01_Simple extends GebReportingTest {
     @Test
     void visit_pragmatists_blog_and_then_main_page() {
         go "http://pragmatists.pl/blog"
 
         // make sure we actually got to the page
         assert title == "Pragmatists Blog"
+
+        report "blog-view"
 
         String mainPageUrl = "http://pragmatists.pl/"
 
