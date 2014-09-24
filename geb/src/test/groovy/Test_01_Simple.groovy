69f80ced9f9639358ed331a8d74623d36d7ffd40
Adam Ruka <adam.ruka@pragmatists.pl>
2014-09-24 10:57:09 +0200

    Basic Pragmatists test using the GebTest JUnit4 integration base class.

--- geb/src/test/groovy/Test_01_Simple.groovy
+++ geb/src/test/groovy/Test_01_Simple.groovy
@@ -1,23 +1,21 @@
-import geb.Browser
+import geb.junit4.GebTest
 import org.junit.Test
 
-class Test_01_Simple {
+class Test_01_Simple extends GebTest {
     @Test
     void visit_pragmatists_blog_and_then_main_page() {
-        Browser.drive {
-            go "http://pragmatists.pl/blog"
+        go "http://pragmatists.pl/blog"
 
-            // make sure we actually got to the page
-            assert title == "Pragmatists Blog"
+        // make sure we actually got to the page
+        assert title == "Pragmatists Blog"
 
-            // extract the banner link
-            def bannerLink = $(text: "Pragmatists")
+        // extract the banner link
+        def bannerLink = $(text: "Pragmatists")
 
-            // click the link
-            bannerLink.click()
+        // click the link
+        bannerLink.click()
 
-            // wait for a redirect to the main page
-            assert currentUrl == "http://pragmatists.pl/"
-        }
+        // wait for a redirect to the main page
+        assert currentUrl == "http://pragmatists.pl/"
     }
 }
